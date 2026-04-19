742dfaa2e69a954dd0c327d948e914816d16bfb4
FCS-dev <107736390+FCS-dev@users.noreply.github.com>
2026-04-19 21:31:02 +0200

    20260419 21:30

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,12 @@
 use App\Http\Controllers\TeacherController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//    return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('teachers',TeacherController::class);
+Route::resource('teachers', TeacherController::class);
+
+// Route::get('/', function () {
+//     return 'OK';
+// });
